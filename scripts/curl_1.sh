05b5ad7ae567d3a8d18a9bcc36689977cdf4831d
dmaclay <dmaclay@gmail.com>
2012-08-03 16:51:52 +0200

    init

---
+++ scripts/curl_1.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+curl -X POST http://localhost:4444 \
+    -H content-type:text/json \
+    -d "{
+            \"action\": \"answer\",
+            \"session-id\": 1,
+            \"answer\": 1
+
+        }"
+
+
